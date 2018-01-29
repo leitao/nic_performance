8a093a6125531b9df0de70bb27a5f5f4646c7e37
Breno Leitao <breno@moog.ltc.br.ibm.com>
2018-01-29 14:19:05 -0200

    DEBUG

--- nic_perf.sh
+++ nic_perf.sh
@@ -4,17 +4,32 @@ IPERF=/usr/bin/iperf
 IPERF3=/usr/bin/iperf3
 TARGET=localhost
 
+#DEBUG=0
+
+if [ -n "$DEBUG" ] ; then
+	echo "DEBUG ENABLED"
+	set -x
+fi
 
 echo Multiples Thread
-for i in 1 2 4 8; do
+for i in 1 3 4 8; do
 	echo -n $i " Thread(s): "
-	RESULT=$($IPERF -c $TARGET -P $i 2> /dev/null | tail -1);
-	echo $RESULT | awk -F'GBytes' '{print $2}'
+	if [ -n "$DEBUG" ] ; then
+		$IPERF -c $TARGET -P $i;
+	else
+		RESULT=$($IPERF -c $TARGET -P $i 2> /dev/null | tail -1);
+		echo $RESULT | awk -F'GBytes' '{print $2}'
+	fi
+		
 done
 
 echo Multiple Stream
 for i in 1 2 8 32; do
 	echo -n $i " Stream (s): "
-	RESULT=$($IPERF3 -c $TARGET -P $i  2> /dev/null | tail -4 | head -1 );
-	echo $RESULT | awk -F"GBytes" '{print $2}' | awk '{print $1 " " $2}'
+	if [ -n "$DEBUG" ] ; then
+		$IPERF3 -c $TARGET -P $i
+	else
+		RESULT=$($IPERF3 -c $TARGET -P $i  2> /dev/null | tail -4 | head -1 );
+		echo $RESULT | awk -F"GBytes" '{print $2}' | awk '{print $1 " " $2}'
+	fi
 done
