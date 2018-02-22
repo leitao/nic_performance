bf7cd9bc7c548d9cfd49870f0090f7a8c0963867
Breno Leitao <brenohl@br.ibm.com>
2018-02-22 08:00:08 -0600

    Use local memory

--- iperf/2ports/first.sh
+++ iperf/2ports/first.sh
@@ -1,3 +1,3 @@
 #!/bin/bash -x
 TIME=${1:-10}
-iperf -c 192.168.1.8 -t $TIME  -P 8 2>&1 > first
+numactl -l iperf -c 192.168.1.8 -t $TIME  -P 8 2>&1 > first
--- iperf/2ports/second.sh
+++ iperf/2ports/second.sh
@@ -1,4 +1,4 @@
 #!/bin/bash -x
 
 TIME=${1:-10}
-iperf -c 10.1.1.8 -t $TIME -P 8 2>&1 > second
+numactl -l iperf -c 10.1.1.8 -t $TIME -P 8 2>&1 > second
