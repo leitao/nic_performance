889658073fa585adb3dfe4c768733256ab4120d5
Breno Leitao <brenohl@br.ibm.com>
2018-02-22 08:31:19 -0600

    dual test not birect

--- iperf/2ports/dual.sh
+++ iperf/2ports/dual.sh
@@ -1,10 +1,15 @@
 #!/bin/bash
-TARGET1=10.1.1.8
-TARGET2=192.168.1.8
+TARGET1=10.1.1.9
+TARGET2=192.168.1.9
 TIME=${1:-10}
+OPTS=""
+
+
+export TARGET1 TARGET2 OPTS
 
 ./first.sh $TIME &
 ./second.sh $TIME  & 
+
 
 sleep 1
 ./cpu_utilization.sh cpu.txt
