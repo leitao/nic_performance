bcd1bff68710f0d85ed02c162c1757b68e7701fa
Breno Leitao <brenohl@br.ibm.com>
2018-02-22 13:06:10 -0600

    getopt

--- wget/dualport/batch_download.sh
+++ wget/dualport/batch_download.sh
@@ -1,21 +1,68 @@
-#!/bin/bash  -x
+#!/bin/bash  
+
+# Arguments: 
 
 TARGET1=10.1.1.9
 TARGET2=192.168.1.9
 FILE=vmlinux
-PARTS=3
+PARTS=8
 HTTP=http
 OUTPUT="-O /dev/null"
 FLAGS=" --no-check-certificate"
 DIR1=nvme1
-DIR2=nvme2
+DIR2=nvme1
+PORTS=2
 
-rm wget-log*
+while getopts ":21rbshp:" opt; do
+  case $opt in
+    b)
+      FILE=bigimage
+      ;;
+    h)
+      echo "Test time: HTTP"
+      HTTP=http
+      ;;
+    s)
+      echo "Test time: HTTPS"
+      HTTP=https
+      ;;
+    p)
+      PARTS=$OPTARG
+      ;;
+    r)
+      DIR1=ramfs
+      DIR2=ramfs
+      ;;
+    2)
+      PORTS=2
+      ;;
+    1)
+      PORTS=1
+      ;;
+    ?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit
+      ;;
+  esac
+done
+
+echo "Benchmark start"
+echo "HTTP method: " $HTTP
+echo "Using " $PARTS " streams per port"
+echo "File: " $FILE
+echo "PORTS: " $PORTS
+
+rm wget-log* 2> /dev/null
 for i in `seq 2 $PARTS`; do
 	wget $FLAGS $HTTP://$TARGET2/$DIR2/$FILE.part_$i $OUTPUT -o wget-log$i &
-	wget $FLAGS $HTTP://$TARGET1/$DIR1/$FILE.part_$i $OUTPUT -o wget-log2$i &
+	if [ "$PORTS" -eq "2" ]; then
+		wget $FLAGS $HTTP://$TARGET1/$DIR1/$FILE.part_$i $OUTPUT -o wget-log2$i &
+	fi
 done
-wget --no-check-certificate $HTTP://$TARGET1/$DIR1/$FILE.part_1 $OUTPUT -o wget-log1
+wget --no-check-certificate $HTTP://$TARGET2/$DIR2/$FILE.part_1 $OUTPUT -o wget-log21
+if [ "$PORTS" -eq "2" ]; then
+	wget --no-check-certificate $HTTP://$TARGET1/$DIR1/$FILE.part_1 $OUTPUT -o wget-log1
+fi
 
 for job in `jobs -p` ; do
     echo "Waiting Job: " $job
@@ -38,5 +85,5 @@ FSPEED=$(echo "scale=2 ; $SPEED * 8 / 1000" | bc)
 echo Final Speed: $FSPEED Gbps 
 
 # removing the files downloaded
-rm $FILE.part_*
+rm $FILE.part_* 2> /dev/null
 
