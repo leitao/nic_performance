66f5b7b37ea03835ec216ca775a3ee271e861757
Breno Leitao <brenohl@br.ibm.com>
2018-03-22 09:12:13 -0500

    tunning CPU

---
+++ prepare.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+iptables -F
+
+echo "NUMA node on POWER9"
+cat /sys/class/net/enP48p1s0f0/device/numa_node
+cat /sys/class/net/enP48p1s0f1/device/numa_node
+
+echo "NUMA node on POWER8:"
+cat /sys/class/net/eth2/device/numa_node
+cat /sys/class/net/eth4/device/numa_node
+
+echo "Setting cpu governor to performance:"
+cpupower frequency-set --governor performance
+
+echo "Setting sysctl parameters"
+sysctl -w net.core.rmem_max=268435456
+sysctl -w net.core.wmem_max=268435456
+
+sysctl -w net.ipv4.tcp_rmem="4096 87380 134217728"
+sysctl -w net.ipv4.tcp_wmem="4096 65536 134217728"
+
+
+sysctl -w net.core.netdev_max_backlog=250000
+
+# don't cache ssthresh from previous connection
+sysctl -w net.ipv4.tcp_no_metrics_save=1
+
+sysctl -w net.ipv4.tcp_congestion_control=htcp
+
+# If you are using Jumbo Frames, also set this
+sysctl -w net.ipv4.tcp_mtu_probing=1
+
+# recommended for CentOS7/Debian8 hosts
+sysctl -w net.core.default_qdisc=fq
