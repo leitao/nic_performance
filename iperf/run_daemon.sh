d95184cb9ec674e9ede914e7243e460386d7c17f
Breno Leitao <brenohl@br.ibm.com>
2018-03-22 14:41:25 -0500

    run iperf -s

--- iperf/run_daemon.sh
+++ iperf/run_daemon.sh
@@ -4,6 +4,11 @@
 killall iperf 2>&1 >/dev/null
 killall iperf3 2>&1 >/dev/null
 
-# restart instances
-iperf -s -D 2>&1 >/dev/null
+#legacy
 iperf3 -s -D 2>&1 >/dev/null
+
+# for 192 connection
+numactl -l -N 0 iperf -s -p 5001 -D 2>&1 >/dev/null
+
+# for 10. connection
+numactl -l -N 8 iperf -s -p 5010 -D 2>&1 >/dev/null
