3741e328f871d6563e27e669400ac965b0120639
Breno Leitao <brenohl@br.ibm.com>
2018-03-22 14:52:48 -0500

    LRO does not help

--- prepare.sh
+++ prepare.sh
@@ -47,8 +47,8 @@ sudo systemctl stop irqbalance
 
 ## Turn on large receive offload
 
-sudo ethtool -K eth4 lro on
-sudo ethtool -K eth2 lro on
-sudo ethtool -K enP48p1s0f0 lro on
-sudo ethtool -K enP48p1s0f1 lro on
+sudo ethtool -K eth4 lro off
+sudo ethtool -K eth2 lro off
+sudo ethtool -K enP48p1s0f0 lro off
+sudo ethtool -K enP48p1s0f1 lro off
 
